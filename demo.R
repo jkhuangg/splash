158034302dcde1154a4fe5f7a215b596e43659e0
Huang <yiju@UMICH.EDU>
2021-01-05 22:25:20 -0500

    add h2o prostate demo

--- demo.R
+++ demo.R
@@ -1,18 +1,35 @@
 library(splash)
+library(h2o)
 
-fn="h2o_lni_glm.zip"
-p="Z:/yiju/splash/mojo_files/"
+# set current working directory
+curr_wd=dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(curr_wd)
+
+# create h2o prostate mojo
+h2o.init(nthreads = -1)
+path <- system.file("extdata", "prostate.csv", package="h2o")
+h2o_df <- h2o.importFile(path)
+h2o_df$CAPSULE <- as.factor(h2o_df$CAPSULE)
+model <- h2o.gbm(y = "CAPSULE",
+                 x = c("AGE", "RACE", "PSA", "GLEASON"),
+                 training_frame = h2o_df,
+                 distribution = "bernoulli",
+                 ntrees = 100,
+                 max_depth = 4,
+                 learn_rate = 0.1)
+
+modelfile <- h2o.download_mojo(model, path= getwd(), get_genmodel_jar=TRUE)
 
 # this is the bare bones version
-splash(fn, p)
+# splash(modelfile, curr_wd)
 
 # specifying all the parameters
-splash(mojo_fn=fn,
-       path=p,
+splash(mojo_fn=modelfile,
+       path=curr_wd,
        output_fn="index.Rmd",
-       title="title1",
-       author="Jake Huang",
-       date="01/03/2021",
+       title="H2O prostate.csv Demo",
+       author="ML4LHS",
+       date="01/05/2021",
        output="html_document",
        runtime="shiny"
        )
