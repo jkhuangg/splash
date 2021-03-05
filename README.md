d99a88b87d3707ed56d08864ac5d70c1f87853e1
Jake Huang <yiju@umich.edu>
2021-03-05 09:23:35 -0500

    edit readme and demo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # splash </br>
 shiny app in demo lives on: </br>
 https://jakehuang.shinyapps.io/splash_prostate_demo/
+
+# New Repo
+This is the original repo for Splash. If you want to see the <strong>official</strong> repo, go to: </br>
+https://github.com/jkhuangg/splash </br>
+
+To check for my authorship: </br>
+https://github.com/ML4LHS/splash/blob/master/DESCRIPTION
--- demo/demo.R
+++ demo/demo.R
@@ -6,26 +6,23 @@ curr_wd=dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(curr_wd)
 
 # create h2o prostate mojo
-# h2o.init(nthreads = -1)
-# path <- system.file("extdata", "prostate.csv", package="h2o")
-# h2o_df <- h2o.importFile(path)
-# h2o_df$CAPSULE <- as.factor(h2o_df$CAPSULE)
-# h2o_df$RACE = as.factor(h2o_df$RACE)
-# model <- h2o.gbm(y = "CAPSULE",
-#                  x = c("AGE", "RACE", "PSA", "GLEASON"),
-#                  training_frame = h2o_df,
-#                  distribution = "bernoulli",
-#                  ntrees = 100,
-#                  max_depth = 4,
-#                  learn_rate = 0.1)
-# 
-# modelfile <- h2o.download_mojo(model, path= getwd(), get_genmodel_jar=TRUE)
+h2o.init(nthreads = -1)
+path <- system.file("extdata", "prostate.csv", package="h2o")
+h2o_df <- h2o.importFile(path)
+h2o_df$CAPSULE <- as.factor(h2o_df$CAPSULE)
+h2o_df$RACE = as.factor(h2o_df$RACE)
+model <- h2o.gbm(y = "CAPSULE",
+                 x = c("AGE", "RACE", "PSA", "GLEASON"),
+                 training_frame = h2o_df,
+                 distribution = "bernoulli",
+                 ntrees = 100,
+                 max_depth = 4,
+                 learn_rate = 0.1)
 
-# this is the bare bones version
-# splash(modelfile, curr_wd)
+modelfile <- h2o.download_mojo(model, path= getwd(), get_genmodel_jar=TRUE)
 
 # specifying all the parameters
-splash(mojo_fn="tx_model_nomets_pub.zip",
+splash(mojo_fn=modelfile,
        path=curr_wd,
        output_fn="index.Rmd",
        title="H2O prostate.csv Demo",
@@ -33,4 +30,8 @@ splash(mojo_fn="tx_model_nomets_pub.zip",
        date="01/05/2021",
        output="html_document",
        runtime="shiny"
-       )
+)
+
+# this is the bare bones version
+# splash(modelfile, curr_wd)
+
